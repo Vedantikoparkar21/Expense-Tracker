b192d3bb5a5f498b9a9c5efff3188909062d515e
Vedanti Koparkar <koparkarvedanti21@gmail.com>
2025-12-23 09:31:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDASDs12sdg3...",
+  authDomain: "cloud-expense-tracker.firebaseapp.com",
+  projectId: "cloud-expense-tracker",
+  storageBucket: "cloud-expense-tracker.appspot.com",
+  messagingSenderId: "42113456789",
+  appId: "1:42113456789:web:xyz123456"
+};
